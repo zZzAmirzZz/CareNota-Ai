c4735de1d727038e341bf977afe9573ce4ae7f89
malakkhaled123 <cds.malakkhaled30407@alexu.edu.eg>
2026-05-14 18:23:12 +0300

    Added preprocessing file + removed language ar in whisper

--- ai/whisper_model.py
+++ ai/whisper_model.py
@@ -1,14 +1,7 @@
-_model = None
+import whisper
 
-def get_model():   #lazy singleton
-    global _model
-    if _model is None:
-        import whisper
-        _model = whisper.load_model("base")
-    return _model
+model = whisper.load_model("base")
 
 def transcribe_audio(file_path):
-    model = get_model()
     result = model.transcribe(file_path, task="transcribe")
     return result["text"]
-
