83d73d28b119d24ba76dc0d117c5b91cc5ba2b05
malakkhaled123 <cds.malakkhaled30407@alexu.edu.eg>
2026-06-08 16:59:44 +0300

    fixed main fast api part

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import uuid
-import httpx
+import requests
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from pydantic import BaseModel
 from ai.pipeline import run_pipeline
+from dotenv import load_dotenv
 
 load_dotenv()
+
 
 os.makedirs("temp", exist_ok=True)
 
@@ -32,25 +34,25 @@ async def process_audio(request: AudioRequest):
     file_path = None
 
     try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(request.audio_url, timeout=15)
-
+        response = requests.get(request.audio_url, timeout=15)
         response.raise_for_status()
 
+        # validate file type
         content_type = response.headers.get("Content-Type", "")
-        if not content_type.startswith("audio"):
+        if "audio" not in content_type:
             return {"error": f"Invalid file type: {content_type}"}
-
-        if len(response.content) > 10 * 1024 * 1024:
-            return {"error": "File too large"}
 
         file_path = f"temp/{uuid.uuid4()}.wav"
 
         with open(file_path, "wb") as f:
             f.write(response.content)
 
-        print("Running pipeline...")
-        result = run_pipeline(file_path)
+        result = run_pipeline(file_path,
+        age=request.age,
+        gender=request.gender,
+        chronic_conditions=request.chronic_conditions,
+        allergies=request.allergies,
+        last_summary=request.last_summary)
 
         return result
 
